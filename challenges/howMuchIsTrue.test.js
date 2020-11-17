0ec72fdf37052fde9be2a532cd9b37a5923d3ace
cbetori <cbetori@gmail.com>
2020-11-17 11:09:23 -0600

    Additional functions and tests, how much is true

--- challenges/howMuchIsTrue.test.js
+++ challenges/howMuchIsTrue.test.js
@@ -2,7 +2,7 @@
 //Return 0 if given an empty array.
 //All array items are of the type bool (true or false).
 
-const countTrueValues = (array) => {
+const countTrueValuesForLoop = (array) => {
 	let result = 0
 	for (let i = 0; i < array.length; i++) {
 		if (array[i]) result++
@@ -10,14 +10,54 @@ const countTrueValues = (array) => {
 	return result
 }
 
+const countTrueValuesReduce = (array) => {
+	if (array.length === 0) return 0
+	let result = 0
+	array.reduce((acc, value, index) => {
+		if (value || acc) {
+			result++
+		}
+	})
+	return result
+}
+
+const countTrueValuesFilter = (array) => {
+	let result = array.filter((value) => value)
+	return result.length
+}
+
 test('how much is true #1', () => {
-	expect(countTrueValues([true, false, false, true, false])).toEqual(2)
+	expect(countTrueValuesForLoop([true, false, false, true, false])).toEqual(2)
 })
 
 test('how much is true #2', () => {
-	expect(countTrueValues([false, false, false, false])).toEqual(0)
+	expect(countTrueValuesForLoop([false, false, false, false])).toEqual(0)
 })
 
 test('how much is true #3', () => {
-	expect(countTrueValues([])).toEqual(0)
+	expect(countTrueValuesForLoop([])).toEqual(0)
+})
+
+test('how much is true #4', () => {
+	expect(countTrueValuesReduce([true, false, false, true, false])).toEqual(2)
+})
+
+test('how much is true #5', () => {
+	expect(countTrueValuesReduce([false, false, false, false])).toEqual(0)
+})
+
+test('how much is true #6', () => {
+	expect(countTrueValuesReduce([])).toEqual(0)
+})
+
+test('how much is true #7', () => {
+	expect(countTrueValuesFilter([true, false, false, true, false])).toEqual(2)
+})
+
+test('how much is true #8', () => {
+	expect(countTrueValuesFilter([false, false, false, false])).toEqual(0)
+})
+
+test('how much is true #9', () => {
+	expect(countTrueValuesFilter([])).toEqual(0)
 })
